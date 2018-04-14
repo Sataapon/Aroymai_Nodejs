b8f636be4427b4f342b0287bc49ad9a9e9bcded7
sataapon <mr.sataapon@gmail.com>
2018-04-14 22:21:01 +0700

    Index.js can get post data, and send it to template

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs')
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.render('index')
+  res.render('index');
 });
 
 app.post('/fill', function(req, res){
-  res.send("Fill page get post request");
+  var foods = {};
+  var drinks = {};
+  for (var key in req.body){
+    if (key[0] == 'F'){
+      foods[key] = req.body[key];
+    }
+    else{
+      drinks[key] = req.body[key];
+    }
+  }
+  res.render('fill', {
+    foods: foods,
+    drinks: drinks
+  });
+});
+
+app.post('/add', function(req, res){
+  res.send("add page get post request");
+  console.log(req.body);
 });
 
 app.post('/review', function(req, res){
-  res.send("Review page get post request");
+  res.send("review page get post request");
+  console.log(req.body);
 });
 
 app.listen(3000);
