3abc09b94a943ef8ea20aa12771b9c059fd15eb4
sataapon <mr.sataapon@gmail.com>
2018-05-29 15:38:33 +0700

    Now index.js used model, Edited templates

--- index.js
+++ index.js
@@ -3,51 +3,106 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res){
-  res.render('home');
+/****** define models ******/
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('database', null, null, {
+  dialect: 'sqlite',
+  storage: './database.sqlite'
 });
 
-app.post('/fill', function(req, res){
-  var foods = {};
-  var drinks = {};
-  for (var key in req.body){
+var Menu = sequelize.define('Menu', {
+  name: Sequelize.STRING,
+  Type: Sequelize.STRING
+});
+
+var User = sequelize.define('User', {
+  name: Sequelize.STRING
+});
+
+var CommentScore = sequelize.define('CommentScore', {
+  comment: Sequelize.TEXT,
+  score: Sequelize.INTEGER
+});
+
+Menu.hasMany(CommentScore);
+User.hasMany(CommentScore);
+/***************************/
+
+app.get('/', function (req, res){
+  Menu.findAll().then(menus => {
+    var foods = {};
+    var drinks = {};
+    for (var i in menus) {
+      if (menus[i].Type == 'Food') {
+        foods[menus[i].name] = menus[i].Type;
+      }
+      else if (menus[i].Type == 'Drink') {
+        drinks[menus[i].name] = menus[i].Type;
+      }
+    }
+    res.render('home', {
+      'foods': foods,
+      'drinks': drinks
+    });
+  });
+});
+
+app.get('/fill', function(req, res){
+  var foods = [];
+  var drinks = [];
+  for (var key in req.query){
     if (key[0] == 'F'){
-      foods[key] = req.body[key];
+      foods.push(req.query[key]);
     }
     else{
-      drinks[key] = req.body[key];
+      drinks.push(req.query[key]);
     }
   }
   res.render('fill', {
-    foods: foods,
-    drinks: drinks
+    'foods': foods,
+    'drinks': drinks
   });
 });
 
 app.post('/add', function(req, res){
-  res.send("add page get post request");
+  var user;
+  if (req.body['User'] == '') {
+    user = 'Noname';
+  }
+  else {
+    user = req.body['User'];
+  }
+  sequelize.sync()
+    .then(() => User.create({ name: user }))
+      .then(user => {
+        console.log('Create user as name', user.dataValues.name);
+        for (var key in req.body){
+          if (key != 'User') {
+            var type_key = key.split('_');
+            console.log(type_key);
+            Menu.findOne({ where: {name: type_key[0]} }).then(menu => {
+              console.log(type_key[1], menu.dataValues.name);
+            });
+          }
+        }
+      });
+
+  res.redirect('/');
 });
 
-app.post('/review', function(req, res){
+app.get('/review', function(req, res){
   res.send(req.body);
 });
 
 app.get('/about', function(req, res){
-  res.sendfile("public/htmls/about.html");
+  res.sendfile('public/htmls/about.html');
 });
 
-app.get('*', function(req, res){
-  res.status(404).send('Sorry, this is an invalid URL.');
-});
-
-var server = app.listen(3000, function () {
-  var port = server.address().port;
-  console.log('Example app listening at port %s', port);
-});
+app.listen(3000);
 
 module.exports = app;
